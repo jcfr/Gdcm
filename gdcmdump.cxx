c1f78f8ed2d7473e46634c22a322abb907e332f7
malat <malat@cb3775c9-7b13-0410-a7d2-ec8aeb55040e>
2007-08-26 16:20:18 +0000

    ENH: start moving to trunk
    git-svn-id: https://gdcm.svn.sourceforge.net/svnroot/gdcm/trunk@1098 cb3775c9-7b13-0410-a7d2-ec8aeb55040e

---
+++ gdcmdump.cxx
@@ -0,0 +1,182 @@
+/*=========================================================================
+
+  Program: GDCM (Grass Root DICOM). A DICOM library
+  Module:  $URL$
+
+  Copyright (c) 2006 Mathieu Malaterre
+  Copyright (c) 1993-2005 CREATIS
+  All rights reserved.
+  See Copyright.txt or http://gdcm.sourceforge.net/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
+#include "gdcmReader.h"
+#include "gdcmFileMetaInformation.h"
+#include "gdcmDataSet.h"
+#include "gdcmPrinter.h"
+#include "gdcmDictPrinter.h"
+#include "gdcmValidate.h"
+#include "gdcmWriter.h"
+
+#include <string>
+#include <iostream>
+
+#include <stdio.h>     /* for printf */
+#include <stdlib.h>    /* for exit */
+#include <getopt.h>
+#include <string.h>
+
+int main (int argc, char *argv[])
+{
+  int c;
+  //int digit_optind = 0;
+
+  std::string filename;
+  bool printdict = false;
+  while (1) {
+    //int this_option_optind = optind ? optind : 1;
+    int option_index = 0;
+    static struct option long_options[] = {
+        {"input", 1, 0, 0},
+        {"output", 1, 0, 0},
+        {"dict", 1, 0, 0},
+        {0, 0, 0, 0}
+    };
+
+    c = getopt_long (argc, argv, "i:o:d:",
+      long_options, &option_index);
+    if (c == -1)
+      {
+      break;
+      }
+
+    switch (c)
+      {
+    case 0:
+        {
+        const char *s = long_options[option_index].name;
+        printf ("option %s", s);
+        if (optarg)
+          {
+          if( option_index == 0 ) /* input */
+            {
+            assert( strcmp(s, "input") == 0 );
+            assert( filename.empty() );
+            filename = optarg;
+            }
+          printf (" with arg %s", optarg);
+          }
+        printf ("\n");
+        }
+      break;
+
+    case 'i':
+      printf ("option i with value '%s'\n", optarg);
+      assert( filename.empty() );
+      filename = optarg;
+      break;
+
+    case 'o':
+      printf ("option o with value '%s'\n", optarg);
+      break;
+
+    case 'd':
+      printf ("option d with value '%s'\n", optarg);
+      printdict = true;
+      break;
+
+    case '?':
+      break;
+
+    default:
+      printf ("?? getopt returned character code 0%o ??\n", c);
+      }
+  }
+
+  if (optind < argc)
+    {
+    printf ("non-option ARGV-elements: ");
+    while (optind < argc)
+      {
+      printf ("%s ", argv[optind++]);
+      }
+    printf ("\n");
+    }
+
+  if( filename.empty() )
+    {
+    std::cerr << "Need input file (-i)\n";
+    return 1;
+    }
+  // else
+  //std::cout << "Filename: " << filename << std::endl;
+  gdcm::Reader reader;
+  reader.SetFileName( filename.c_str() );
+  if( !reader.Read() )
+    {
+    std::cerr << "Failed to read: " << filename << std::endl;
+    return 1;
+    }
+
+  gdcm::Validate vali;
+  vali.SetFile( reader.GetFile() );
+  vali.Validation();
+
+  gdcm::Printer *p = 0;
+  gdcm::Printer     printer;
+  //std::cerr << "PRINTDICT:" << printdict << std::endl;
+  if( printdict )
+  {
+	  //	  FIXME: need virtual mechanism
+//	  p = &dictprinter;
+  gdcm::DictPrinter dictprinter;
+  dictprinter.SetFile ( reader.GetFile() );
+  dictprinter.Print( std::cout );
+  return 0;
+  }
+  else
+  {
+	  p = &printer;
+  }
+  // TODO:
+
+  p->SetFile( reader.GetFile() );
+  //p->SetFile( vali.GetValidatedFile() );
+  //const gdcm::FileMetaInformation &h = reader.GetHeader();
+  ////std::cout << h << std::endl;
+  //if(!h.IsEmpty())
+  //{
+  ////printer.SetDataSet( h );
+  ////printer.Print( std::cout );
+  //}
+
+ //const gdcm::DataSet &ds = reader.GetDataSet();
+  ////std::cout << ds << std::endl;
+  //printer.SetDataSet( reader.GetDataSet() );
+  p->Print( std::cout );
+  //std::cout << reader.GetFile() << std::endl;
+
+//  std::ofstream of;
+//  of.open( "/tmp/valii.dcm", std::ios::out | std::ios::binary );
+//  const char line[] = "coucou mathieu";
+//  //of.write( line, strlen(line) );
+//  of << line;
+//  of.close();
+//  gdcm::Writer writer;
+//  writer.SetFileName( "/tmp/vali2.dcm" );
+//  writer.SetFile( reader.GetFile() );
+//  //writer.SetFile( vali.GetValidatedFile() );
+//  if( !writer.Write() )
+//    {
+//    std::cerr << "Failed to write: "  << std::endl;
+//    return 1;
+//    }
+
+
+  return 0;
+}
+
---
+++ gdcmrewrite.cxx
@@ -0,0 +1,175 @@
+/*=========================================================================
+
+  Program: GDCM (Grass Root DICOM). A DICOM library
+  Module:  $URL$
+
+  Copyright (c) 2006 Mathieu Malaterre
+  Copyright (c) 1993-2005 CREATIS
+  All rights reserved.
+  See Copyright.txt or http://gdcm.sourceforge.net/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+/*
+ * Tools to rewrite. Goals being to 'purify' a DICOM file.
+ * For now it will do the minimum:
+ * - If Group Length is present, the length is garantee to be correct
+ * - If Element with Group Tag 0x1, 0x3, 0x5 or 0x7 are present they are
+ *   simply discarded (not written).
+ * - Elements are written in alphabetical order
+ * - 32bits VR have the residue bytes sets to 0x0,0x0
+ * - Same goes from Item Length end delimitor, sets to 0x0,0x0
+ * - All buggy files (wrong length: GE, 13 and Siemens Leonardo) are fixed
+ * - All size are even (no odd length from gdcm 1.x)
+ *
+ *
+ * \todo in a close future:
+ * - Set appropriate VR from DICOM dict
+ * - Rewrite PMS SQ into DICOM SQ
+ * - Rewrite Implicit SQ with defined length as undefined length
+ * - PixelData with `overlay` in unused bits should be cleanup
+ * - Any broken JPEG file (wrong bits) should be fixed
+ * - DicomObject bug should be fixed
+ * - Meta and Dataset should have a matching UID (more generally File Meta
+ *   should be correct (Explicit!) and consistant with DataSet)
+ * - User should be able to specify he wants Group Length (or remove them)
+ * - Media SOP should be correct (deduct from something else or set to
+ *   SOP Secondary if all else fail).
+ * - Padding character should be correct
+ *
+ * \todo distant future:
+ * - Later on, it should run through a Validator
+ *   which will make sure all field 1, 1C are present and those only
+ * - In a perfect world I should remove private tags and transform them into
+ *   public fields.
+ * - DA should be correct, PN should be correct (no space!)
+ * - Enumerated Value should be correct
+ */
+#include "gdcmReader.h"
+#include "gdcmWriter.h"
+#include "gdcmFileMetaInformation.h"
+#include "gdcmDataSet.h"
+
+#include <string>
+#include <iostream>
+
+#include <stdio.h>     /* for printf */
+#include <stdlib.h>    /* for exit */
+#include <getopt.h>
+#include <string.h>
+
+int main (int argc, char *argv[])
+{
+  int c;
+  //int digit_optind = 0;
+
+  std::string filename;
+  std::string outfilename;
+  while (1) {
+    //int this_option_optind = optind ? optind : 1;
+    int option_index = 0;
+    static struct option long_options[] = {
+        {"input", 1, 0, 0},
+        {"output", 1, 0, 0},
+        {0, 0, 0, 0}
+    };
+
+    c = getopt_long (argc, argv, "i:o:",
+      long_options, &option_index);
+    if (c == -1)
+      {
+      break;
+      }
+
+    switch (c)
+      {
+    case 0:
+        {
+        const char *s = long_options[option_index].name;
+        printf ("option %s", s);
+        if (optarg)
+          {
+          if( option_index == 0 ) /* input */
+            {
+            assert( strcmp(s, "input") == 0 );
+            assert( filename.empty() );
+            filename = optarg;
+            }
+          printf (" with arg %s", optarg);
+          }
+        printf ("\n");
+        }
+      break;
+
+    case 'i':
+      printf ("option i with value '%s'\n", optarg);
+      assert( filename.empty() );
+      filename = optarg;
+      break;
+
+    case 'o':
+      printf ("option o with value '%s'\n", optarg);
+      assert( outfilename.empty() );
+      outfilename = optarg;
+      break;
+
+    case '?':
+      break;
+
+    default:
+      printf ("?? getopt returned character code 0%o ??\n", c);
+      }
+  }
+
+  if (optind < argc)
+    {
+    printf ("non-option ARGV-elements: ");
+    while (optind < argc)
+      {
+      printf ("%s ", argv[optind++]);
+      }
+    printf ("\n");
+    }
+
+  if( filename.empty() )
+    {
+    std::cerr << "Need input file (-i)\n";
+    return 1;
+    }
+  // else
+  //std::cout << "Filename: " << filename << std::endl;
+  gdcm::Reader reader;
+  reader.SetFileName( filename.c_str() );
+  if( !reader.Read() )
+    {
+    std::cerr << "Failed to read: " << filename << std::endl;
+    return 1;
+    }
+
+  //const gdcm::FileMetaInformation &h = reader.GetHeader();
+  //std::cout << h << std::endl;
+
+  //const gdcm::DataSet &ds = reader.GetDataSet();
+  //std::cout << ds << std::endl;
+
+  if( outfilename.empty() )
+    {
+    std::cerr << "Need output file (-o)\n";
+    return 1;
+    }
+
+  gdcm::Writer writer;
+  writer.SetFileName( outfilename.c_str() );
+  writer.SetFile( reader.GetFile() );
+  if( !writer.Write() )
+    {
+    std::cerr << "Failed to write: " << outfilename << std::endl;
+    return 1;
+    }
+
+  return 0;
+}
+
